fe6d6a75d25c6300d9f93be51a7d7e9c6bae8fc0
Jeronimo Sturniolo <jeronimosturniolo44@gmail.com>
2025-01-13 17:23:17 -0300

    Fin de la seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+
+  public title:string = 'Hola mundo';
+  public counter:number = 10;
+
+  increaseBy(valor:number):void{
+    this.counter += valor;
+  }
+
+  reset():void{
+    this.counter = 10;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
